9deb3e6e80afa77908549a31f8ea526b5b9f5037
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-17 16:34:42 +0530

    Code Commit

--- Sol_Demo/Sol_Demo/Program.cs
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -9,6 +9,37 @@ namespace Sol_Demo
         {
             Console.WriteLine("Hello World!");
 
+            // Basic Null_Coalescing_Operator
+            string item_1 = null;
+            string item_2 = null;
+            string item_3 = "Hello C#";
+
+            string result = null;
+
+            result = item_1 ?? item_2 ?? item_3;
+
+            Console.WriteLine(item_1); // NulL
+            Console.WriteLine(item_2); // Null
+            Console.WriteLine(item_3); // Hello C#
+            Console.WriteLine(result); // Hello C#
+
+            // Basic Null_Coalescing_ Assignment Operator
+            string item_4 = null;
+            string item_5 = null;
+            string item_6 = "Hello C#";
+
+            result = item_4 ??= item_5 ??= item_6;
+
+            Console.WriteLine(item_4); // Hello C#
+            Console.WriteLine(item_5); // Hello C#
+            Console.WriteLine(item_6); // Hello C#
+            Console.WriteLine(result); // Hello C#
+
+
+
+
+
+
             // using Null_Coalescing_Operator
             List<String> list = null;
 
